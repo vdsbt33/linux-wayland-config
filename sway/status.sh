8b904ab002f0d35a638485f6ba8e6585044d4062
Vinícius de Souza <vdsbt33@gmail.com>
2023-12-28 15:14:00 -0300

    Initial commit.

---
+++ sway/status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+uptime_formatted=$(uptime | cut -d ',' -f1 | cut -d ' ' -f4,5)
+
+date_formatted=$(date "+%a %F %H:%M")
+
+output_volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Eo '[0-9]{1,3}\%' | head -1)
+output_icon="\uf028" # normal
+if [[ $(pactl get-sink-mute @DEFAULT_SINK@) =~ "yes" ]]; then
+	output_icon="\ueb24" # muted
+fi
+
+input_volume=$(pactl get-source-volume @DEFAULT_SOURCE@ | grep -Eo '[0-9]{1,3}%' | head -1)
+input_icon="\uf130" # normal
+if [[ $(pactl get-source-mute @DEFAULT_SOURCE@) =~ "yes" ]]; then
+	input_icon="\uf131" # muted
+fi
+
+echo -e "$uptime_formatted \ueb2d   $output_volume $output_icon   $input_volume $input_icon   $date_formatted "

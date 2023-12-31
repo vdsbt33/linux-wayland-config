94bee824d9b306d256a7f2a09478ba913f388f0d
Vinícius de Souza <vdsbt33@gmail.com>
2023-12-30 23:44:52 -0300

    Added week day. Screenshot and auto-close Firefox's Sharing Indicator.

--- sway/status.sh
+++ sway/status.sh
@@ -2,6 +2,7 @@
 
 uptime_formatted=$(uptime | cut -d ',' -f1 | cut -d ' ' -f4,5)
 
+weekday_formatted=$(date "+%A")
 date_formatted=$(date "+%d/%m/%Y")
 time_formatted=$(date "+%H:%M")
 
@@ -17,4 +18,4 @@ if [[ $(pactl get-source-mute @DEFAULT_SOURCE@) =~ "yes" ]]; then
 	input_icon="\uf131" # muted
 fi
 
-echo -e "$uptime_formatted \ueb2d   $output_volume $output_icon   $input_volume $input_icon   $date_formatted \ueab0   $time_formatted \ue382 "
+echo -e "\ueb2d $uptime_formatted   $output_icon $output_volume   $input_icon $input_volume   \ueab0 $date_formatted ($weekday_formatted)   \ue382 $time_formatted "
